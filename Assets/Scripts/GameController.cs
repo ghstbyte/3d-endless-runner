5b24a09a40ec478ed0ccba3697270aaedbe3e944
ghstbyte <qsftygit72jhk@gmail.com>
2025-08-13 23:09:13 +0300

    Add the ability to collect coins

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
-public class EndGame : MonoBehaviour
+public class GameController : MonoBehaviour
 {
     [SerializeField] private CharacterController _playerCharacterController;
+    [SerializeField] private PlayerController _playerController;
     [SerializeField] private GameObject _endGameUI;
+    public TextMeshProUGUI _coinsText;
+    private int _coins = 0;
     private void Awake()
     {
         _playerCharacterController = GetComponent<CharacterController>();
@@ -16,6 +20,16 @@ public class EndGame : MonoBehaviour
         {
             _endGameUI.SetActive(true);
             Time.timeScale = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider takeCoin)
+    {
+        if (takeCoin.gameObject.CompareTag("Coins"))
+        {
+            _coins++;
+            Destroy(takeCoin.gameObject);
+            _coinsText.text =$"Coins: {_coins}";
         }
     }
 
