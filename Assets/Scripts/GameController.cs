b43a972860378e590482ee303d40acdea570b890
ghstbyte <qsftygit72jhk@gmail.com>
2025-08-15 05:40:36 +0300

    Add record score and update menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,9 +8,28 @@ public class GameController : MonoBehaviour
     [SerializeField] private CharacterController _playerCharacterController;
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private GameObject _endGameUI;
+    [SerializeField] private Score _scoreScript;
+    [SerializeField] private TMP_Text _recordScoreText;
     public TextMeshProUGUI _coinsText;
     private int _coins = 0;
     private int _totalCoins = 0;
+
+    private void Start()
+    {
+        int lastRunScore = PlayerPrefs.GetInt("lastRunScore");
+        int recordScore = PlayerPrefs.GetInt("recordScore");
+        if (recordScore < lastRunScore)
+        {
+            recordScore = lastRunScore;
+            PlayerPrefs.SetInt("recordScore", recordScore);
+            PlayerPrefs.Save();
+            _recordScoreText.text = $"Record: {recordScore.ToString()}";
+        }
+        else
+        {
+            _recordScoreText.text = $"Record: {recordScore.ToString()}";
+        }
+    }
 
     private void Awake()
     {
@@ -22,8 +41,10 @@ public class GameController : MonoBehaviour
         if (hit.collider.CompareTag("Obstacle"))
         {
             _endGameUI.SetActive(true);
-            Time.timeScale = 0f;
             SaveCoins(_coins);
+            int _lastRunScore = int.Parse(_scoreScript._scoreText.text);
+            PlayerPrefs.SetInt("lastRunScore", _lastRunScore);
+            Time.timeScale = 0f;
         }
     }
 
@@ -49,5 +70,10 @@ public class GameController : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
     }
 }
