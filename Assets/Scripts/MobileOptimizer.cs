c9ce066aeba91443c52d762a0c74bd16b0b61111
ghstbyte <qsftygit72jhk@gmail.com>
2025-08-19 00:01:45 +0300

    Fix mobile optimization

--- Assets/Scripts/MobileOptimizer.cs
+++ Assets/Scripts/MobileOptimizer.cs
@@ -2,36 +2,8 @@ using UnityEngine;
 
 public class MobileOptimizer : MonoBehaviour
 {
-    private static MobileOptimizer instance;
-
-    private int originalWidth;
-    private int originalHeight;
-    private bool resolutionSet = false;
-
     void Awake()
     {
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        instance = this;
-        DontDestroyOnLoad(gameObject);
-
-        QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
-
-        originalWidth = Screen.currentResolution.width;
-        originalHeight = Screen.currentResolution.height;
-    }
-
-    void Start()
-    {
-        if (!resolutionSet)
-        {
-            Screen.SetResolution(originalWidth / 2, originalHeight / 2, true);
-            resolutionSet = true;
-        }
     }
 }
